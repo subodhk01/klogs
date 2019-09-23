49cee420af0dd997958ae618f28bb07b2c5de03c
Subodhk <vermasubodhk@gmail.com>
2019-09-23 16:29:42 +0530

    settings.py changes(timezone and allowed_hosts)

--- klogs/settings.py
+++ klogs/settings.py
@@ -106,13 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'ASIA/Calcutta'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
+ALLOWED_HOSTS = ['127.0.0.1', '.pyhtonanywhere.com']
 
 
 # Static files (CSS, JavaScript, Images)
